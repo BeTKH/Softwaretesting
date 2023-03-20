54b0fd09e523d346add41cdd8472932a38ba9c60
Bekalue <bekalue.tkh@gmail.com>
2023-03-20 08:08:25 -0500

    parametrized testing with NUnit

---
+++ UnitTesting_withC#/ParametrizedTesting/ParametrizedTest/MatH.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametrizedTest
+{
+    public class MatH
+    {
+        public int Add(int a, int b) 
+        {
+            return a + b;
+        }
+
+        public int Sub(int a, int b) 
+        {
+            return a - b;
+        }
+
+        public int Mul(int a, int b) 
+        {
+            return a * b;
+        }
+
+        public double Div(double a, double b) 
+        {
+            return a / b;
+        }
+
+        public int Max(int a, int b) 
+        {
+            return (a > b) ? a : b;
+        }
+
+        public IEnumerable<IEquatable<int>> GetOddNumbers(int limit) 
+        {
+            for (var i = 0; i <= limit; i++)
+                if (i%2 != 0)
+                    yield return i;
+        }
+    }
+}
