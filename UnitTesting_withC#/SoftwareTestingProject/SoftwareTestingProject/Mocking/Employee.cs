1ff57fe1411636e2cde29e3595937c2e4da23cbf
Bekalue <bekalue.tkh@gmail.com>
2023-03-23 21:57:54 -0500

    Mocking to isolate dpeendencies for unit-testing

---
+++ UnitTesting_withC#/SoftwareTestingProject/SoftwareTestingProject/Mocking/Employee.cs
@@ -0,0 +1,31 @@
+﻿
+// Employee abstract base class.
+using System.Net.Sockets;
+namespace SoftwareTestingProject.Mocking
+{
+    public abstract class Employee
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string SocialSecurityNumber { get; }
+        public Address HomeAddress { get; set; }
+
+        // three-parameter constructor
+        public Employee(string firstName, string lastName,
+           string socialSecurityNumber, Address _address)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            SocialSecurityNumber = socialSecurityNumber;
+            HomeAddress= _address;
+        }
+
+        // return string representation of Employee object, using properties
+        public override string ToString() => $"{FirstName} {LastName}\n" +
+           $"social security number: {SocialSecurityNumber}";
+
+        // abstract method overridden by derived classes
+        public abstract decimal Earnings(); // no implementation here
+    }
+
+}
---
+++ UnitTesting_withC#/SoftwareTestingProject/SoftwareTestingProject/Mocking/SalariedEmployee.cs
@@ -0,0 +1,45 @@
+﻿
+// SalariedEmployee class that extends Employee.
+using System;
+using SoftwareTestingProject.Mocking;
+
+public class SalariedEmployee : Employee
+{
+   private decimal weeklySalary;
+
+   // four-parameter constructor
+   public SalariedEmployee(string firstName, string lastName,
+      string socialSecurityNumber, decimal weeklySalary,Address _address)
+      : base(firstName, lastName, socialSecurityNumber, _address)
+   {
+      WeeklySalary = weeklySalary; // validate salary via property
+   }
+
+   // property that gets and sets salaried employee's salary
+   public decimal WeeklySalary
+   {
+      get
+      {
+         return weeklySalary;
+      }
+      set
+      {
+         if (value < 0) // validation
+         {
+            throw new ArgumentOutOfRangeException(nameof(value),
+               value, $"{nameof(WeeklySalary)} must be > 0");
+         }
+
+         weeklySalary = value;
+      }
+   }
+
+   // calculate earnings; override abstract method Earnings in Employee
+   public override decimal Earnings() => WeeklySalary;
+
+   // return string representation of SalariedEmployee object
+   public override string ToString() =>
+      $"salaried employee: {base.ToString()}\n" +
+      $"weekly salary: {WeeklySalary:C}";
+}
+
