4ccb8047185385c271ba761aa77cb0e3a5ebc9f0
Bekalu Tadesse <45674779+BeTKH@users.noreply.github.com>
2023-04-03 19:42:41 -0500

    dependency injection for testing

---
+++ UnitTesting_withC#/SoftwareTestingProject/SoftwareTestingProject/DependencyInjection/TaxCalculator1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareTestingProject.DependencyInjection
+{
+    public class TaxCalculator1: ITaxCalculator
+    {
+        private int taxableIncome;
+
+        // constructor
+        TaxCalculator1(int taxableIncome_) 
+        {
+            this.taxableIncome = taxableIncome_;
+        }
+
+        // method
+        override
+        public double CalculateTax() 
+        {
+            return taxableIncome *0.45;
+        }
+
+    }
+}
---
+++ UnitTesting_withC#/SoftwareTestingProject/SoftwareTestingProject/DependencyInjection/TaxCalculator2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareTestingProject.DependencyInjection
+{
+    public class TaxCalculator2: ITaxCalculator
+    {
+        private int taxableIncome;
+
+        // constructor
+        TaxCalculator2(int taxableIncome_)
+        {
+            this.taxableIncome = taxableIncome_;
+        }
+
+        // method
+        override
+        public double CalculateTax()
+        {
+            return taxableIncome * 0.15;
+        }
+    }
+}
